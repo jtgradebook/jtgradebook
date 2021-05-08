94b4c90b4f9d24d0deafc4bcdaafaa995474ff3a
Jeremy R. Simpson <simpsonjr@me.com>
2021-05-08 21:00:22 +0000

    Adding timestamp to Comments

--- flask_app.py
+++ flask_app.py
@@ -3,8 +3,9 @@
 
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import login_required, login_user, LoginManager, logout_user, UserMixin
+from flask_login import current_user, login_required, login_user, LoginManager, logout_user, UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -62,7 +63,9 @@ class Comment(db.Model):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=Comment.query.all())
+        return render_template("main_page.html", comments=Comment.query.all(), timestamp=datetime.now())
+    if not current_user.is_authenticated:
+        return redirect(url_for('index'))
 
     comment = Comment(content=request.form["contents"])
     db.session.add(comment)
